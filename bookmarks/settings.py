35b137895d736df8d51f68ac584f7922b6632c3c
Juliano <juliano.co.lopes@gmail.com>
2024-09-19 08:29:41 -0300

    feat: made header menu with login/logout

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -100,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
